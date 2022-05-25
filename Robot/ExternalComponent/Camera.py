8de8b08bbfaf5647e7eeb56e5bc7ca6482ee49f1
Psychopath24 <m.hooijberg24@gmail.com>
2022-05-25 14:30:24 +0200

    Added:   - Camera class first prototype

--- Robot/ExternalComponent/Camera.py
+++ Robot/ExternalComponent/Camera.py
@@ -1,9 +1,30 @@
+import asyncio
+import cv2 as cv
+import numpy as np
+
+
 class Camera:
     def __init__(self):
-        pass
+        self.frameList = []
+        self.loop = asyncio.get_event_loop()
 
-    def StartStream():
-        pass
+    def GetFrame(self, frameIndex):
+        return self.frameList[frameIndex]
 
-    def StopStream():
-        pass
+    async def __StreamLoop_Async(self):
+        while True:
+            _, frame = self.cap.read()
+            if (len(self.frameList) < 72):
+                self.frameList.append(frame)
+            else:
+                for x in range(1, 72):
+                    self.frameList.insert(x - 1, self.frameList[x])
+                self.frameList[71] = frame
+
+    def StartStream(self):
+        self.cap = cv.VideoCapture(0)
+        asyncio.ensure_future(__StreamLoop_Async())
+        self.loop.run_forever()
+
+    def StopStream(self):
+        self.loop.stop()
