af85271690c68243b38b58ececf844f45a922e98
Medooosa <70697922+Medooosa@users.noreply.github.com>
2022-05-31 15:02:51 +0200

    Implement ability to send data to display, can send Text and Emotion

--- Robot/ExternalComponent/Screen.py
+++ Robot/ExternalComponent/Screen.py
@@ -1,10 +1,39 @@
+from ..Types.EmotionState import EmotionState
+import serial
+
 class Screen:
 
     def __init__(self):
         pass
 
-    def DisplayText(self, text):
-        pass
+    def __SendToDisplay(self, commando):
+        ser = serial.Serial(
+            port='/dev/serial0',
+            baudrate =115200,           
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+            )
+        data = bytes([commando])
+        ser.write(data)
 
-    def DisplayImage(self, image):
-        pass
+    def DisplayEmotion(self, emotion):
+        match emotion:
+            case EmotionState.Angry:
+                self.__SendToDisplay("Angry")
+            case EmotionState.Happy:
+                self.__SendToDisplay("Happy")
+            case EmotionState.Sad:
+                self.__SendToDisplay("Sad")
+            case EmotionState.Neutral:
+                self.__SendToDisplay("Neutral")
+            case EmotionState.Tired:
+                self.__SendToDisplay("Tired")
+
+    #TODO Volg deze guide:https://tutorials-raspberrypi.com/digital-raspberry-pi-scale-weight-sensor-hx711/ 
+    # en calibreer de gewichtssensor en importeer de library.
+    def DisplayText(self):
+        val = max(0, int(hx.get_weight(5)))
+        self.__SendToDisplay(val) 
+
