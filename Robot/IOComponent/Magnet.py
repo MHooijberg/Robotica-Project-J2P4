f1d23eb57e275b19f0f5799a42396b379e574409
Maurits.P <maurits.peereboom@student.nhlstenden.com>
2022-05-31 10:58:06 +0200

    Added: simple turnon & turnoff function for magnet

--- Robot/IOComponent/Magnet.py
+++ Robot/IOComponent/Magnet.py
@@ -1,4 +1,13 @@
+import RPi.GPIO as GPIO
 class Magnet:
 
     def __init__(self):
-        pass
+        Mag_Pin = 20
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(Mag_Pin, GPIO.OUT)
+    
+    def turnON(self):
+        GPIO.output(self.Mag_Pin, GPIO.HIGH)
+    
+    def turnOFF(self):
+        GPIO.output(self.Mag_Pin, GPIO.LOW)
