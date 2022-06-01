f1a24118e7077b8329bb04003eeee07813ab811b
Psychopath24 <m.hooijberg24@gmail.com>
2022-06-01 09:19:15 +0200

    fixed commands

--- Robot/ExternalComponent/Screen.py
+++ Robot/ExternalComponent/Screen.py
@@ -1,6 +1,7 @@
 from ..Types.EmotionState import EmotionState
 import serial
 
+
 class Screen:
 
     def __init__(self):
@@ -9,31 +10,30 @@ class Screen:
     def __SendToDisplay(self, commando):
         ser = serial.Serial(
             port='/dev/serial0',
-            baudrate =115200,           
+            baudrate=115200,
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS,
             timeout=1
-            )
+        )
         data = bytes([commando])
         ser.write(data)
 
     def DisplayEmotion(self, emotion):
         match emotion:
             case EmotionState.Angry:
-                self.__SendToDisplay("Angry")
+                self.__SendToDisplay("page angry")
             case EmotionState.Happy:
-                self.__SendToDisplay("Happy")
+                self.__SendToDisplay("page happy")
             case EmotionState.Sad:
-                self.__SendToDisplay("Sad")
+                self.__SendToDisplay("page sad")
             case EmotionState.Neutral:
-                self.__SendToDisplay("Neutral")
+                self.__SendToDisplay("page neutral")
             case EmotionState.Tired:
-                self.__SendToDisplay("Tired")
+                self.__SendToDisplay("page tired")
 
-    #TODO Volg deze guide:https://tutorials-raspberrypi.com/digital-raspberry-pi-scale-weight-sensor-hx711/ 
+    # TODO Volg deze guide:https://tutorials-raspberrypi.com/digital-raspberry-pi-scale-weight-sensor-hx711/
     # en calibreer de gewichtssensor en importeer de library.
     def DisplayText(self):
         val = max(0, int(hx.get_weight(5)))
-        self.__SendToDisplay(val) 
-
+        self.__SendToDisplay(val)
