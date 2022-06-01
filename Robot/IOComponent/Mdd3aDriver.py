d88dcc1501c6c38b1c74458d42420664f4adabd2
Psychopath24 <m.hooijberg24@gmail.com>
2022-06-01 15:37:52 +0200

    Removed:     - Squashed bugs now the motors actually work

--- Robot/IOComponent/Mdd3aDriver.py
+++ Robot/IOComponent/Mdd3aDriver.py
@@ -24,11 +24,6 @@ class Mdd3aDriver:
         GPIO.setup(self.pins[2], GPIO.OUT)
         GPIO.setup(self.pins[3], GPIO.OUT)
 
-        GPIO.output(self.pins[0], GPIO.LOW)
-        GPIO.output(self.pins[1], GPIO.LOW)
-        GPIO.output(self.pins[2], GPIO.LOW)
-        GPIO.output(self.pins[3], GPIO.LOW)
-
         self.pwmPin1a = GPIO.PWM(self.pins[0], 20000)
         self.pwmPin1b = GPIO.PWM(self.pins[1], 20000)
         self.pwmPin2a = GPIO.PWM(self.pins[2], 20000)
@@ -41,47 +36,47 @@ class Mdd3aDriver:
 
     def Backward(self, speedLeft, speedRight):
         self.pwmPin1b.start(speedLeft)
-        GPIO.output(self.pins[0], GPIO.LOW)
+        self.pwmPin1a.ChangeDutyCycle(0)
         self.pwmPin2b.start(speedRight)
-        GPIO.output(self.pins[2], GPIO.LOW)
+        self.pwmPin2a.ChangeDutyCycle(0)
 
     def Brake(self):
-        GPIO.output(self.pins[0], GPIO.LOW)
-        GPIO.output(self.pins[1], GPIO.LOW)
-        GPIO.output(self.pins[2], GPIO.LOW)
-        GPIO.output(self.pins[3], GPIO.LOW)
+        self.pwmPin1a.ChangeDutyCycle(0)
+        self.pwmPin1b.ChangeDutyCycle(0)
+        self.pwmPin2a.ChangeDutyCycle(0)
+        self.pwmPin2b.ChangeDutyCycle(0)
 
     def Forward(self, speedLeft, speedRight):
         self.pwmPin1a.start(speedLeft)
-        GPIO.output(self.pins[1], GPIO.LOW)
+        self.pwmPin1b.ChangeDutyCycle(0)
         self.pwmPin2a.start(speedRight)
-        GPIO.output(self.pins[3], GPIO.LOW)
+        self.pwmPin2b.ChangeDutyCycle(0)
 
     # Rotate around the midpoint with both motors, to the right.
     def RotateLeft(self, speed):
         self.pwmPin1a.start(speed)
-        GPIO.output(self.pins[1], GPIO.LOW)
+        self.pwmPin1b.ChangeDutyCycle(0)
         self.pwmPin2b.start(speed)
-        GPIO.output(self.pins[2], GPIO.LOW)
+        self.pwmPin2a.ChangeDutyCycle(0)
 
     # Rotate around the midpoint with both motors, to the right.
 
     def RotateRight(self, speed):
         self.pwmPin1b.start(speed)
-        GPIO.output(self.pins[0], GPIO.LOW)
+        self.pwmPin1a.ChangeDutyCycle(0)
         self.pwmPin2a.start(speed)
-        GPIO.output(self.pins[3], GPIO.LOW)
+        self.pwmPin2b.ChangeDutyCycle(0)
 
     # Only the left wheel spins.
     def TurnLeft(self, speed):
         self.pwmPin1a.start(speed)
-        GPIO.output(self.pins[1], GPIO.LOW)
-        GPIO.output(self.pins[2], GPIO.LOW)
-        GPIO.output(self.pins[3], GPIO.LOW)
+        self.pwmPin1b.ChangeDutyCycle(0)
+        self.pwmPin2a.ChangeDutyCycle(0)
+        self.pwmPin2b.ChangeDutyCycle(0)
 
     # Only the right wheel spins.
     def TurnRight(self, speed):
         self.pwmPin2a.start(speed)
-        GPIO.output(self.pins[3], GPIO.LOW)
-        GPIO.output(self.pins[0], GPIO.LOW)
-        GPIO.output(self.pins[1], GPIO.LOW)
+        self.pwmPin1a.ChangeDutyCycle(0)
+        self.pwmPin1b.ChangeDutyCycle(0)
+        self.pwmPin2b.ChangeDutyCycle(0)
