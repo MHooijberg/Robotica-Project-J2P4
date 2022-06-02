f5f19a32f94a403fc4f5a55358acaa443f3515e9
Martijn-UI <Martijn.Koning1@student.nhlstenden.com>
2022-06-02 15:49:55 +0200

    html website

--- Robot/Socket/BluetoothSocket.py
+++ Robot/Socket/BluetoothSocket.py
@@ -1,3 +1,18 @@
-class RemoteSocket:
-    def __init__(self):
-        pass
+import socket
+
+MACAddress = E4-AA-EA-BF-F3-74
+port = 246924
+connection = socket.socket(socket.AF_BLUETOOTH, socket_SOCK_STREAM, socket.BTPROTO_RFCOMM)
+
+connection.bind((MACAddress, port))
+connection.listen(1)
+try:
+	client, address = connection.accept()
+	while 1:
+		ReceivedData = client.recv(1024)
+		if ReceiveData:
+			print(ReceiveData)
+except:
+	print("Closing Socket")
+	client.close()
+	connection.close()
