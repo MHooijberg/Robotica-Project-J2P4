7e6d93102fe9cee7188432d871955b745a83f9d2
Maurits.P <maurits.peereboom@student.nhlstenden.com>
2022-06-03 13:40:01 +0200

    Added: Speaker functionallity (kind of)

---
+++ Robot/ExternalComponent/Speaker.py
@@ -0,0 +1,10 @@
+from playsound import playsound
+  
+# TODO: Add the right URL Paths
+# Keep in mind that this library only supports .wav and .mp3 files
+
+def Greet():
+    playsound('/Voices/Hello.mp3', False)
+
+def Pitch():
+    playsound('/Voices/Pitch.mp3', False)
