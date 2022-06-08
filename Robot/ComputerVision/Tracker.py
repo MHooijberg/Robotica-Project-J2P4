e8b0d6a2d86831bca5748bc949dcc6eb05ced8cd
Medooosa <70697922+Medooosa@users.noreply.github.com>
2022-06-08 14:51:54 +0200

    Half blueblock tracking fix

--- Robot/ComputerVision/Tracker.py
+++ Robot/ComputerVision/Tracker.py
@@ -1,8 +1,7 @@
 import cv2 as cv
 from cv2 import WINDOW_NORMAL
 import numpy as np
-import Robot.Types.TrackMode as TrackMode
-import Robot.Types.ObjectPosition as ObjectPosition
+
 
 
 class Tracker:
@@ -10,25 +9,24 @@ class Tracker:
         self.cap = cv.VideoCapture(0)
 
     def GetPositionTrackingObject(self):
-
         # while True:
-        _, self.frame = self.cap.read()
+        _, frame = self.cap.read()
+        #frame = rawFrame
 
         width = 500
-
         height = 500
 
         # Convert BGR to HSV
-        hsv = cv.cvtColor(self.frame, cv.COLOR_BGR2HSV)
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         # define range of blue color in HSV
         #lower_color_filter = np.array([82, 150, 100])
         #upper_color_filter = np.array([90, 255, 255])
-        lower_color_filter = np.array([100, 206, 30])
-        upper_color_filter = np.array([130, 255, 180])
+        lower_color_filter = np.array([77, 87, 64])
+        upper_color_filter = np.array([88, 182, 215])
         # Threshold the HSV image to get only the specified colorsq
         mask = cv.inRange(hsv, lower_color_filter, upper_color_filter)
         # Bitwise-AND mask and original image
-        res = cv.bitwise_and(self.frame, self.frame, mask=mask)
+        res = cv.bitwise_and(frame, frame, mask=mask)
 
         contours, hierarchy = cv.findContours(
             mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -46,16 +44,22 @@ class Tracker:
                 cy = int(M['m01']/M['m00'])
 
                 if area > 400:
-                    cv.drawContours(self.frame, hull, -1, (0, 0, 255), 2)
-                    cv.circle(self.frame, (cx, cy), 5, (0, 255, 255), -1)
+                    cv.drawContours(frame, hull, -1, (0, 0, 255), 2)
+                    cv.circle(frame, (cx, cy), 5, (0, 255, 255), -1)
 
                 #print("X:", cx, "Y:", cy)
             if cx > 450:
-                return ObjectPosition.left
+                print("The object position was: Left")
+                #return
+                #return ObjectPosition.left
             if cx < 200:
-                return ObjectPosition.right
+                print("The object position was: Right")
+                #return
+                #return ObjectPosition.right
             if cx < 450 and cx > 200:
-                return ObjectPosition.middle
+                print("The object position was: Middle")
+                #return
+                #return ObjectPosition.middle
 
                 # if cx>450:
                 #     print("left")
@@ -64,8 +68,8 @@ class Tracker:
                 # if cx<450 and cx>200:
                 #     print("middle")
 
-        # cv.namedWindow('frame', WINDOW_NORMAL)
-        # cv.resizeWindow('frame', (width, height))
+        #cv.namedWindow('frame', WINDOW_NORMAL)
+        #cv.resizeWindow('frame', (width, height))
 
         # cv.imshow('frame', frame)
         # cv.imshow('mask', mask)
