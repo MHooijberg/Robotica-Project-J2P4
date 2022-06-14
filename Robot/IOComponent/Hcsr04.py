3c3a290905f2643c30a162f4bf7655b14d0531e9
Maurits.P <maurits.peereboom@student.nhlstenden.com>
2022-06-14 16:36:38 +0200

    Added:
    - Notes consistency ~ distanceSensor

--- Robot/IOComponent/Hcsr04.py
+++ Robot/IOComponent/Hcsr04.py
@@ -3,12 +3,39 @@ import time
 
 class Hcsr04:
 
+# ================
+# ---- Notes: ----
+# ================
+#
+#
+# TODO: Test this code with the HCSR04 distance sensor
+#
+# --Function--
+#
+# The goal of this class is to communicate with the HCSR04 distance sensor
+# Not only does it receive the data from the sensor, this function also calculates this data in cm
+# Please keep in mind that this class only returns the measured data between the censor and the obstacle in centimeters
+# It does not return directions for the wheels, this needs to be done outside of this class
+#
+#
+# --How to use--
+#
+# 1. call the 'findTarget' function, which will almost immeadially call the 'createRadar' function
+# 2. the purpose for the 'createRadar' method is to first create a radar, which it uses to check diferences in height on a terrain
+#    when the difference is under the average of the radar, this class will classify it as an object
+#    this method will return a black image on which a surten number of objects may be located
+#    these objects will be represented as thick, red lines
+# 3. after calling the 'localizeJunk' method, the findTarget will first check if there are any items at all
+#    if there are found items, it will search for the biggest target and draw that target on an emty black image (otherwise 'No items found' will be returned)
+#    finally, it will determine if the target is at left, or the right of the middle of the screen (but not before checking if the target is in front of the screen)
+#
+
     # initialisation method, this is used to setup global variables.
     def __init__(self, pinA, pinB):
         GPIO.setmode(GPIO.BCM)
         # recommended pinA = 18
         self.GPIO_TRIGGER = pinA
-        # recommended pinA = 24
+        # recommended pinB = 24
         self.GPIO_ECHO = pinB
         # set GPIO direction (IN / OUT)
         GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
